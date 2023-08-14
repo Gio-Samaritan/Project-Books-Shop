76673a235e31d5cc5f9bed3a50f09add45d00b51
Gio-Samaritan <107702671+Gio-Samaritan@users.noreply.github.com>
2023-08-14 18:33:04 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,33 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html=webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
- 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: [{
-          directory: path.join(__dirname, "dist"),
-        }]
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
     },
-    watch: true,
     output: {
-        filename: '[name].[contenthash].js', // динамичное и уникальное имя файла
-        path: path.resolve(__dirname, 'dist'),
-        clean: true, // для очистки папки dist при новом билде
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
     },
     plugins: [
-        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Наш заголовок страницы',
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
         }),
     ],
-    module: {
-        rules: [
-            /** Babel **/
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-                // npm install babel-loader @babel/core @babel/preset-env -D
-            },
-            /** CSS */
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-                // npm i style-loader css-loader -D
-            },
-            /** SCSS/SAAS */
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-                // npm i style-loader css-loader sass sass-loader -D
-            },
-            /** Картинки */
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            /** Шрифты */
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(?:js|mjs|cjs)$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: [
-                      ['@babel/preset-env', { targets: "defaults" }]
-                    ]
-                  }
-                }
-              }
-        ],
-    },
-};
-
+    rules: [
+        // JavaScript
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+        },
+        {
+            test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+            type: 'asset/resource',
+        }
+    ],
+}
